daae7dce1fd5770b56b7fe7a00f0d6b2fe6c5bb1
johnsiefert <johnsief12@gmail.com>
2022-01-08 14:02:17 -0500

    added public folder

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
-  secret: 'Kratos',
+  secret: 'Super secret secret',
   cookie: {},
   resave: false,
   saveUninitialized: true,
